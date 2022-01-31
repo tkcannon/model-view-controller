c8ff60e99bb495dbb52027d4cf051a49fd40ea09
tkcannon <taylorkcannon@gmail.com>
2022-01-31 15:23:20 -0700

    added models

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const sequelize = require('./config/connection');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Server Now Listening'));
+})
