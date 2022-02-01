c31d08439d7752fa964d000028bb6b8a6699dff3
tkcannon <taylorkcannon@gmail.com>
2022-01-31 18:34:14 -0700

    added api routes

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-const express = require('express')
-const sequelize = require('./config/connection');
 const path = require('path');
+const express = require('express');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Server Now Listening'));
