e5d60f15bd9778e6f8fd3c7a7bd483ef923e3581
tkcannon <taylorkcannon@gmail.com>
2022-02-04 13:30:22 -0700

    added comment inclusion on post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -8,6 +8,14 @@ router.get('/', (req, res) => {
         attributes: ['id', 'title', 'text', 'created_at'],
         include: [
             {
+                model: Comment,
+                attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
                 model: User,
                 attributes: ['username']
             }
@@ -27,10 +35,20 @@ router.get('/:id', (req, res) => {
             id: req.params.id
         },
         attributes: ['id', 'title', 'text', 'created_at'],
-        include: {
-            model: User,
-            attributes: ['username']
-        }
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
         .then(postData => {
             if (!postData) {
