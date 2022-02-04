863ea8752a6dced3892be8e645883967a9b37f41
tkcannon <taylorkcannon@gmail.com>
2022-02-04 13:31:33 -0700

    added missing Comment reference in post-routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 // get all
 router.get('/', (req, res) => {
