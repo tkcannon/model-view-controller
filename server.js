4fe7ed0f0f02a4538371800aaa64c66a0200a8f5
tkcannon <taylorkcannon@gmail.com>
2022-02-06 04:17:27 -0700

    added session timeout

--- server.js
+++ server.js
@@ -11,9 +11,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 300000
+  },
   resave: false,
   saveUninitialized: true,
+  rolling: true,
   store: new SequelizeStore({
     db: sequelize
   })
